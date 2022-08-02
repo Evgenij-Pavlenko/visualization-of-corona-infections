372c050df7666881c4d8f63be4c353ca90187427
Ievgenii Pavlenko <evgenij.pavlenko@gmail.com>
2022-08-03 00:10:07 +0200

    start2

---
+++ src/main/java/com/j/visualizationofcoronainfections/VisualizationOfCoronaInfectionsApplication.java
@@ -0,0 +1,13 @@
+package com.j.visualizationofcoronainfections;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisualizationOfCoronaInfectionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisualizationOfCoronaInfectionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/j/visualizationofcoronainfections/VisualizationOfCoronaInfectionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.j.visualizationofcoronainfections;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VisualizationOfCoronaInfectionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
